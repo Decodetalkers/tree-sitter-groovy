9fb574a1544bb32c4d1e42ef93cbe4138a553b06
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2023-05-05 15:14:42 +0800

    fix: wrong binding

--- bindings/node/binding.cc
+++ bindings/node/binding.cc
@@ -4,7 +4,7 @@
 
 using namespace v8;
 
-extern "C" TSLanguage * tree_sitter_groovyscript();
+extern "C" TSLanguage * tree_sitter_groovy();
 
 namespace {
 
@@ -17,12 +17,12 @@ void Init(Local<Object> exports, Local<Object> module) {
 
   Local<Function> constructor = Nan::GetFunction(tpl).ToLocalChecked();
   Local<Object> instance = constructor->NewInstance(Nan::GetCurrentContext()).ToLocalChecked();
-  Nan::SetInternalFieldPointer(instance, 0, tree_sitter_groovyscript());
+  Nan::SetInternalFieldPointer(instance, 0, tree_sitter_groovy());
 
-  Nan::Set(instance, Nan::New("name").ToLocalChecked(), Nan::New("groovyscript").ToLocalChecked());
+  Nan::Set(instance, Nan::New("name").ToLocalChecked(), Nan::New("groovy").ToLocalChecked());
   Nan::Set(module, Nan::New("exports").ToLocalChecked(), instance);
 }
 
-NODE_MODULE(tree_sitter_groovyscript_binding, Init)
+NODE_MODULE(tree_sitter_groovy_binding, Init)
 
 }  // namespace
